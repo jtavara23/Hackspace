4c91b2bdd79d3e090d31cbff51b9feac5c249c86
jtavara23 <joshuatavara23@gmail.com>
2017-12-03 00:22:49 -0500

    heroku version

--- server.js
+++ server.js
@@ -27,8 +27,10 @@ require('./routers')(server);
 
 // CONFIGURACIONES DB
 // Integración de mongoose
-mongoose.connect('mongodb://localhost/hackspace', { useMongoClient: true });
+//mongoose.connect('mongodb://localhost/hackspace', { useMongoClient: true });
+mongoose.connect('mongodb://jtavara23:JGti2323@ds129641.mlab.com:29641/hackspace', { useMongoClient: true });
 mongoose.Promise = global.Promise
+
 
 // INICIAR SERVIDOR
 // Se corre el servidor en el puerto 8000
@@ -37,6 +39,7 @@ server.listen(8000, function() {
 	console.log('El servidor esta escuchando en el puerto '+ 8000)
 }); 
 */
+
 server.listen(process.env.PORT || 5000, function () {
 	console.log('El servidor esta escuchando en el puerto ' + 5000)
 });
